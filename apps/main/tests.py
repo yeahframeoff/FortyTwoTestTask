f439e11ffe0cf1de9b5da524f72bc5a33052bb5e
Anton Yefremov <anthyef@gmail.com>
2015-02-27 01:43:36 +0200

    ticket:4 added tests for settings in context

--- apps/main/tests.py
+++ apps/main/tests.py
@@ -1,7 +1,18 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
 class SomeTests(TestCase):
     def test_math(self):
         assert(2 + 2 == 4)
 
+
+class SettingsContextProcessorTests(TestCase):
+    def test_request_context_has_settings_in_it(self):
+        """
+        Response for any non 404 page should have 'settings'
+        in its context
+        """
+        response = self.client.get(reverse('main'))
+        if response.status_code != 404 :
+            self.assertIn('settings', response.context)
