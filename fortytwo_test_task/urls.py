664122d59a21f04beb6bb20bb6f695fef921cccf
Anton Yefremov <anthyef@gmail.com>
2015-03-02 08:41:08 +0200

    ticket:5 added view for edit page

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = patterns(
@@ -11,4 +12,5 @@ urlpatterns = patterns(
     url(r'^$', 'main.views.mainpage', name='main'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^requests/', 'requests_saver.views.requestspage', name='requests'),
-)
+    url(r'^userdata/', 'main.views.editpage', name='edit'),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
